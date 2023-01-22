7b0509ac54f40969b2e2346259cba52995873c39
rg98098 <rg98098@gmail.com>
2023-01-22 08:04:01 +0530

    resturant menu

---
+++ class_eight/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About us Page</h1>
+      <p>
+        This is about us page
+      </p>
+    </div>
+  )
+}
+
+export default About

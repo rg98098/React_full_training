bdf4499171963d01295b1971edc2317da073cbd4
rg98098 <rg98098@gmail.com>
2023-01-25 13:40:57 +0530

    practice

--- class_eight/src/components/About.js
+++ class_eight/src/components/About.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react";
+// import { Outlet } from 'react-router-dom'
+import Profile from "./ProfileClass";
+import ProfileFunctionalComponent from "./Profile";
+class About extends React.Component {
+  constructor(props){
+    super(props);
 
-const About = () => {
-  return (
-    <div>
-      <h1>About us Page</h1>
-      <p>
-        This is about us page
-      </p>
-    </div>
-  )
+    // console.log("Parent constructor")
+  }
+  // call api inside in this, because this will call after render
+ async componentDidMount(){
+
+    // console.log("Parent componentDidMount")
+  }
+
+  render() {
+    // console.log("Parent render")
+    return (
+      <div>
+        <h1>About us Page</h1>
+        <p>This is about us page</p>
+        <ProfileFunctionalComponent name={"Rahul"} />
+        {/* <Profile name={"First child"} /> */}
+        {/* <Profile name={"Second child"} /> */}
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;

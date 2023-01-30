4de76f94a4a81ee41f469a4d5bc402471a6cc198
rg98098 <rg98098@gmail.com>
2023-01-30 23:09:48 +0530

    tailwind css

--- class_ten/src/components/ResturantCard.js
+++ class_ten/src/components/ResturantCard.js
@@ -3,12 +3,12 @@ import { IMG_CON_URL } from "../constants";
 const ResturantCard = ({ name, lastMileTravelString, cloudinaryImageId, cuisines }) => {
 
   return (
-    <div className="card">
+    <div className="w-56 p-2 m-2 shadow-lg bg-pink-50">
       <img
         src={`${IMG_CON_URL}${cloudinaryImageId}`}
         alt={name}
       />
-      <h2>{name}</h2>
+      <h2 className="font-bold text-xl">{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
       <h4>{lastMileTravelString} minutes</h4>
     </div>
